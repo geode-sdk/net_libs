d37597fdd73d2a9ea48c7097485a23969dbe3b99
kynex7510 <nikkitten3@gmail.com>
2024-05-30 12:18:50 +0200

    Modified script

--- build_android.sh
+++ build_android.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
 ROOT=$PWD
-#ABI="arm64-v8a"
-ABI="armeabi-v7a"
+ABI="arm64-v8a"
+#ABI="armeabi-v7a"
 SDK_VER=23
-BUILD_PATH="$ROOT/build/android32"
-OUT_PATH="$ROOT/out/android32"
+BUILD_PATH="$ROOT/build/android64"
+OUT_PATH="$ROOT/out/android64"
 
 # Remove previous output files
 
-rm -rf "$OUT_PATH"
+#rm -rf "$OUT_PATH"
 
 # Build BoringSSL
 
-rm -rf "$BUILD_PATH/boringssl"
-mkdir -p "$BUILD_PATH/boringssl"
-cd "$BUILD_PATH/boringssl"
+#rm -rf "$BUILD_PATH/boringssl"
+#mkdir -p "$BUILD_PATH/boringssl"
+#cd "$BUILD_PATH/boringssl"
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$OUT_PATH" \
-    -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake" \
-    -DANDROID_ABI=$ABI -DANDROID_PLATFORM=android-$SDK_VER "$ROOT/boringssl"
-make -j$(nproc)
-make install
-make clean
+#cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$OUT_PATH" -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+#    -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake" \
+#    -DANDROID_ABI=$ABI -DANDROID_PLATFORM=android-$SDK_VER "$ROOT/boringssl"
+#make -j$(nproc)
+#make install
+#make clean
 
 # Build nghttp2
 
-rm -rf "$BUILD_PATH/nghttp2"
-mkdir -p "$BUILD_PATH/nghttp2"
-cd "$BUILD_PATH/nghttp2"
+#rm -rf "$BUILD_PATH/nghttp2"
+#mkdir -p "$BUILD_PATH/nghttp2"
+#cd "$BUILD_PATH/nghttp2"
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$OUT_PATH" -DENABLE_LIB_ONLY=ON -DENABLE_EXAMPLES=OFF \
-    -DBUILD_TESTING=OFF -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake" -DANDROID_ABI=$ABI \
-    -DANDROID_PLATFORM=android-$SDK_VER -DBUILD_SHARED_LIBS=OFF -DBUILD_STATIC_LIBS=ON "$ROOT/nghttp2"
-make -j$(nproc)
-make install
-make clean
+#cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$OUT_PATH" -DENABLE_LIB_ONLY=ON -DENABLE_EXAMPLES=OFF \
+#    -DBUILD_TESTING=OFF -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake" -DANDROID_ABI=$ABI \
+#    -DANDROID_PLATFORM=android-$SDK_VER -DBUILD_SHARED_LIBS=OFF -DBUILD_STATIC_LIBS=ON "$ROOT/nghttp2"
+#make -j$(nproc)
+#make install
+#make clean
 
 # Build ngtcp2
 
@@ -43,7 +43,11 @@ make clean
 #mkdir -p "$BUILD_PATH/ngtcp2"
 #cd "$BUILD_PATH/ngtcp2"
 
-#"$ROOT/ngtcp2/configure" --host $TARGET --prefix="$OUT_PATH" --disable-shared
+#cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$OUT_PATH" -DBUILD_TESTING=OFF \
+#    -DENABLE_BORINGSSL=ON -DBORINGSSL_INCLUDE_DIR="$OUT_PATH/include" \
+#    -DBORINGSSL_LIBRARIES="$OUT_PATH/lib/libcrypto.a;$OUT_PATH/lib/libssl.a" \
+#    -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake" -DANDROID_ABI=$ABI \
+#    -DANDROID_PLATFORM=android-$SDK_VER -DENABLE_SHARED_LIB=OFF -DENABLE_STATIC_LIB=ON "$ROOT/ngtcp2"
 #make -j$(nproc)
 #make install
 #make clean
@@ -57,9 +61,12 @@ cd "$BUILD_PATH/curl"
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$OUT_PATH" -DBUILD_CURL_EXE=OFF \
     -DCURL_USE_OPENSSL=ON -DOPENSSL_INCLUDE_DIR="$OUT_PATH/include" \
     -DOPENSSL_CRYPTO_LIBRARY="$OUT_PATH/lib/libcrypto.a" -DOPENSSL_SSL_LIBRARY="$OUT_PATH/lib/libssl.a" \
+    -DOPENSSL_LIBRARIES="$OUT_PATH/lib/libcrypto.a;$OUT_PATH/lib/libssl.a" -DCMAKE_REQUIRED_LINK_OPTIONS="-lc++" \
     -DUSE_NGHTTP2=ON -DNGHTTP2_INCLUDE_DIR="$OUT_PATH/include" -DNGHTTP2_LIBRARY="$OUT_PATH/lib/libnghttp2.a" \
+    -DUSE_NGTCP2=ON -DNGTCP2_INCLUDE_DIR="$OUT_PATH/include" -DNGTCP2_LIBRARY="$OUT_PATH/lib/libngtcp2.a" \
+    -Dngtcp2_crypto_boringssl_LIBRARY="$OUT_PATH/lib/libngtcp2_crypto_boringssl.a" \
     -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake" -DANDROID_ABI=$ABI \
     -DANDROID_PLATFORM=android-$SDK_VER -DBUILD_SHARED_LIBS=OFF -DBUILD_STATIC_LIBS=ON "$ROOT/curl"
-make -j$(nproc)
-make install
-make clean
+#make -j$(nproc)
+#make install
+#make clean
