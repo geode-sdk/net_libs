ca3858d8a392aef68573f2223280a7b07e575f19
larzie <lunar@lavache.com>
2024-12-19 16:24:39 +0300

    add spaces

--- make_ca_bundle.py
+++ make_ca_bundle.py
@@ -5,6 +5,8 @@ content = urlopen("https://curl.se/ca/cacert.pem").read().decode()
 with open("ca_bundle.h", "w") as out:
     out.write("#pragma once\n\n")
     out.write("constexpr static char CA_BUNDLE_CONTENT[] =")
+
     for line in content.split('\n'):
         out.write(f'\n"{line}\\n"')
+
     out.write(';')
